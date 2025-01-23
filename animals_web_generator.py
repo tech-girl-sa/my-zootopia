a03850e48ee38bcf435d3bcada0b1929dca39c84
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-01-23 11:06:23 +0100

    move serializing single animal to separate function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,23 +5,27 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-def serialize_animals_cards(data):
+def serialize_animal(animal_obj):
+    to_display = {
+        "Name": animal_obj.get("name"),
+        "Diet": animal_obj['characteristics'].get('diet'),
+        "Location": animal_obj.get("locations")[0],
+        "Type": animal_obj['characteristics'].get('type')
+    }
+    animal_card = '<li class="cards__item">\n'
+    for key, value in to_display.items():
+        if key == "Name":
+            animal_card += f'<div class="card__title">{value}</div>\n <p class="card__text">'
+        elif value:
+            animal_card += f"<strong>{key}</strong>: {value}<br/>\n"
+    animal_card += '</p>\n</li>\n'
+    return animal_card
+
+def serialize_animals(data):
     animals =""
     for animal in data:
-        to_display = {
-            "Name": animal.get("name"),
-            "Diet": animal['characteristics'].get('diet'),
-            "Location": animal.get("locations")[0],
-            "Type":animal['characteristics'].get('type')
-        }
-        display_text = '<li class="cards__item">\n'
-        for key, value in to_display.items():
-            if key == "Name":
-                display_text += f'<div class="card__title">{ value }</div>\n <p class="card__text">'
-            elif value:
-                display_text += f"<strong>{ key }</strong>: { value }<br/>\n"
-        display_text += '</p>\n</li>\n'
-        animals += display_text
+        animal_card = serialize_animal(animal)
+        animals += animal_card
     return animals
 
 def load_template(html_path):
@@ -30,7 +34,7 @@ def load_template(html_path):
 
 def regenerate_html(html_path, data):
     content = load_template(html_path)
-    animals_display = serialize_animals_cards(data)
+    animals_display = serialize_animals(data)
     new_content = content.replace("__REPLACE_ANIMALS_INFO__", animals_display)
     with open(html_path,"w") as handle:
         handle.write(new_content)
